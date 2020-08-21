ef4e65c20532cd12e73a219ed601ef213864f2a0
Alexis LAFRANCE <alexislafrances-laf@outlook.fr>
2020-08-21 23:05:19 +0200

    ft_split done and C10 ex00 done too

---
+++ C10/ex00/ft_display_file.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+void ft_putchar(char c){
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str){
+	int i;
+
+	i = 0;
+	while(str[i])
+		ft_putchar(str[i++]);
+}
+void ft_display_file(char *filename){
+	int fd;
+	char buf[1];
+
+	fd = open(filename, O_RDONLY);
+
+	if(fd == -1)
+		ft_putstr("Cannot read file.\n");
+	else{
+		while(read(fd, buf, 1))
+			ft_putstr(buf);
+		close(fd);
+	}
+}
+
+int main(int ac, char **av){
+	if(ac == 1)
+		ft_putstr("File name missing.\n");
+	else if(ac > 2)
+		ft_putstr("Too many arguments.\n");
+	else
+		ft_display_file(av[1]);
+}
